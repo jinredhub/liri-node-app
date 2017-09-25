e3679904107d91b01f514c472772cef2c4a39cc3
jinredhub <jin@nieblo.com>
2017-09-25 12:25:28 -0700

    added command my-tweet, spotify-this-song, movie-this

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'M3IqvAQiEIIVWncu7c81FL3MU',
+  consumer_secret: 'Jryb016eGg8OraN3TwiyNgEwxdISnwB5zSirS45zmBjxQe5Vfx',
+  access_token_key: '912089731533385728-cQcQLBXrMUosbAEVK9GOxD4TOeTjpdX',
+  access_token_secret: 'rWn1es7EdOYMwjnNY98MXGbQjf8XjhZ3JIAOhB2V4IsEv',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,92 @@
+
+// spotify dev. you need these to use npm node-spotify-api
+// client id 2c4d0610c6bf4949b28f722bc92848f7
+// client secret fe9b91861db543b09832aece6cf3570a
+
+var twitterKeys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var request = require("request");
+// choose command
+var arg1 = process.argv[2];
+//song name or movie name
+var arg2 = process.argv[3];
+
+if (arg1 === "my-tweets"){
+
+	var client = new Twitter(
+		twitterKeys
+	);
+	var params = {screen_name: 'Mike Red'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    // console.log(tweets);
+	   	// console.log(JSON.stringify(tweets, null, 2));
+	   	console.log(tweets[0].text);
+	  }
+	});
+}
+else if (arg1 === "spotify-this-song"){
+	// Artist(s)
+	// The song's name
+	// A preview link of the song from Spotify
+	// The album that the song is from
+	// * if user didn't provide song, output "The Sign" by Ace of Base
+	var spotify = new Spotify({
+	  id: "2c4d0610c6bf4949b28f722bc92848f7",
+	  secret: "fe9b91861db543b09832aece6cf3570a"
+	});
+	 
+	spotify.search({ type: 'track', query: arg2, limit: 1}, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+	 
+	console.log(JSON.stringify(data, null, 2)); 
+	console.log("Artist: ", data.tracks.items[0].album.artists[0].name);
+	console.log("Name of song: ", data.tracks.items[0].name);
+	console.log("Preview link of song: ", data.tracks.items[0].preview_url);
+	console.log("Album: ", data.tracks.items[0].album.name);
+	});
+}
+else if (arg1 === "movie-this"){
+	// Title of the movie.
+	// Year the movie came out.
+	// IMDB Rating of the movie.
+	// Rotten Tomatoes Rating of the movie.
+	// Country where the movie was produced.
+	// Language of the movie.
+	// Plot of the movie.
+	// Actors in the movie.
+	// * if user doesn't type movie in, output data for movie "Mr. Nobody"
+	// * you may use OMDB api key 40e9cece
+	console.log("else if : movie-this");
+	var queryURL = "http://www.omdbapi.com/?apikey=40e9cece&t=" + arg2;
+	
+	request(queryURL, function(err, response, body){
+		// console.log(body);
+		console.log(JSON.parse(body).Title);
+		console.log(JSON.parse(body).Year);
+		console.log(JSON.parse(body).imdbRating);
+		console.log(JSON.parse(body).Ratings[0].Value);
+		console.log(JSON.parse(body).Language);
+		console.log(JSON.parse(body).Country);
+		console.log(JSON.parse(body).Plot);
+		console.log(JSON.parse(body).Actors);
+
+
+	});
+}
+else if (arg1 === "do-what-it-says"){
+	// use fs to read random.txt file
+	// then run spotify-this-song from random.txt file
+	var fs = require("fs");
+	fs.fileRead("random.txt", "utf-8", function(err, data){
+		if (err){
+			console.log(err);
+		}
+		else{
+
+		}
+	})
+}
